a5efdedca46122932929c5402879a3d671c5d529
Egidijus Bockus <egidijusbockus@gmail.com>
2021-09-05 20:35:04 +0300

    Expanding effect

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,1 +1,16 @@
+"use strict";
 
+const panels = document.querySelectorAll(".panel");
+
+const removeActiveClasses = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
